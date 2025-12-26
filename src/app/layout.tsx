4d88098693a6729fa0395ba0e51dabf22cb277e6
SEOSiri <admin@seosiri.com>
2025-12-26 20:55:43 +0600

    fix(ui): Remove third-party chat widget from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,19 +116,6 @@ export default function RootLayout({
               </div>
             </footer>
           )}
-  <Script id="tawk-to-script" strategy="lazyOnload">
-            {`
-                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/694a2f1e9914c8197bb907a2/1jd4sg4ac';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-                })();
-            `}
-        </Script>
 
         <CookieBanner />
       </body>
